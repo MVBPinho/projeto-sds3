bc3c5a810a29b65c68af30a5b5ad50e41bbe423f
MVBPinho <marcos.vb.pinho@live.com>
2021-05-06 20:36:24 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/MVBPinho" target="_blank" rel="noreferrer">Marcos Pinho</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/marcos.pinho/" target="_blank" rel="noreferrer">@marcos.pinho</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
