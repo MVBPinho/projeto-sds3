56746856d5c4ff0e3fd208d6d462fd5785c7ee49
MVBPinho <marcos.vb.pinho@live.com>
2021-05-08 23:34:18 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,5 @@
+import { Seller } from "./seller"
+
 export type SaleSum = {
     sellerName: string,
     sum: number
@@ -7,4 +9,25 @@ export type SaleSuccess = {
     sellerName: string;
     visited: number;
     deals: number;
+}
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
 }
